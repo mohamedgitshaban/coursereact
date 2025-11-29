b61c6003baf1bb50ecb38439669cc506f7a5c6c7
mohamed-shaban-dafa <mohamed.shabaan@dafagate.com>
2025-11-29 17:03:01 +0200

    start ecomerce project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import NavbarComponent from './components/NavbarComponent';
-import HomePage from './pages/HomePage';
-import TablePage from './pages/TablePage';
-import { BrowserRouter, Route, Routes } from 'react-router';
-
 function App() {
   
   return (
-    <BrowserRouter>
-      <NavbarComponent />
-      <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/table" element={<TablePage />} />
-        </Routes> 
-    </BrowserRouter>
+    <div></div>
   );
 }
 
